91995055307a80eb7ec5ab6162a10e2b5f5c8761
JohnYanxinLiu <johnliuchs2022@gmail.com>
2024-06-27 20:05:07 -0400

    kernel and libc dirs

---
+++ kernel/kernel/kernel.d
@@ -0,0 +1,5 @@
+kernel/kernel.o: kernel/kernel.c \
+ /home/jliu/Development/LiuOS/sysroot/usr/include/stdio.h \
+ /home/jliu/Development/LiuOS/sysroot/usr/include/sys/cdefs.h \
+ include/kernel/tty.h \
+ /home/jliu/opt/cross/lib/gcc/i686-elf/12.2.0/include/stddef.h
---
+++ libc/stdio/putchar.libk.d
@@ -0,0 +1,3 @@
+stdio/putchar.libk.o: stdio/putchar.c include/stdio.h include/sys/cdefs.h \
+ /home/jliu/Development/LiuOS/sysroot/usr/include/kernel/tty.h \
+ /home/jliu/opt/cross/lib/gcc/i686-elf/12.2.0/include/stddef.h
